8f39a6ac85530c08b5af1eb7902b5d4356566f1e
Karim Mohamed <komicho1996@gmail.com>
2025-09-30 20:19:17 +0300

    first commit

---
+++ src/ObelawAttrifyServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Obelaw\Attrify;
+
+use Illuminate\Support\ServiceProvider;
+
+class ObelawAttrifyServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
